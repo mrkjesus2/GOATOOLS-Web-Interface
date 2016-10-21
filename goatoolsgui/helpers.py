a5e211c5a5db4b4a39474d987ccf5820b5e55565
Mark <markweig@gmail.com>
2016-10-21 11:25:08 -0400

    Add GrouperDflts() on startup to speed development

--- goatoolsgui/helpers.py
+++ goatoolsgui/helpers.py
@@ -8,20 +8,24 @@ import os
 # End comment
 
 import goatools
-from goatools_alpha.gosubdag_grouper import GrouperUserGos, wr_xlsx_gos, wr_xlsx_gos_sectionsfile, wr_sections_txt
-from goatools_alpha import gosubdag_grouper as gt
+from goatools_alpha import gosubdag_grouper as gta
 from datetime import datetime
 import json
+from django.core.cache import cache
 
-# print os.getcwd()
-# os.chdir('/var/www/projects/gosite/data_files/')
-# flat_groups = gt.GrouperDflts()
+
+# Here to speed development
+os.chdir('/var/www/projects/gosite/data_files/')
+FLAT_GROUPS = gta.GrouperDflts()
+os.chdir('/var/www/projects/gosite/')
+# /speed development
+
 
 # TODO: Pass a boolean stating wheter a file download is wanted?
 def submit_gos(form, form_file_path=None):
   goids = str(form.get('goids')).translate(None, ',').split()
   filename = str(form.get('filename'))
-  print(goids)
+  # print(goids)
 
   """
   Any file that is passed will be a 'sections' file and must be passed to
@@ -33,7 +37,7 @@ def submit_gos(form, form_file_path=None):
     print('')
     # TODO: Check for 'flat' or 'section' key
     # Return found key or file to download?
-    data = wr_xlsx_gos_sectionsfile(filename, goids, form_file_path)
+    data = gta.wr_xlsx_gos_sectionsfile(filename, goids, form_file_path, grprdflt=FLAT_GROUPS)
     print(data['flat'])
     return data['flat']
 
@@ -45,7 +49,7 @@ def submit_gos(form, form_file_path=None):
     print('\nNo file sent to submit_gos\n')
     # TODO: Check for 'flat' or 'sections' key
     # Return found key or the file to download
-    data = wr_xlsx_gos(filename, goids)
+    data = gta.wr_xlsx_gos(filename, goids, grprdflt=FLAT_GROUPS)
     return data
 
 # Accepts 'flat' or 'sections' key in submit_gos() data object
@@ -69,12 +73,12 @@ def nts_to_json(data):
   # print(response_object['flat'][0]) # Access one table row
 
 #
-def make_sections_file(section_name, goids, grprdflt=FLAT_GROUPS):
-  start_time = datetime.now()
-  group_obj = GrouperUserGos(section_name, goids)
-  group_obj.grprobj.wr_txt_section_hdrgos(section_name + '_hdrgos.txt')
-  end_time = datetime.now()
-  print '\n'
-  print end_time - start_time
-  print '\n'
-  return
+# def make_sections_file(section_name, goids, grprdflt=FLAT_GROUPS):
+#   start_time = datetime.now()
+#   group_obj = gta.GrouperUserGos(section_name, goids)
+#   group_obj.grprobj.wr_txt_section_hdrgos(section_name + '_hdrgos.txt')
+#   end_time = datetime.now()
+#   print '\n'
+#   print end_time - start_time
+#   print '\n'
+#   return
