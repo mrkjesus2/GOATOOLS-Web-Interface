7c44bd57a9e5100d7e791070e227363756435643
Mark <markweig@gmail.com>
2016-11-01 21:28:53 -0400

    Add socket server calls to models.py
    The calls to the socket server were added as methods on the GoId class
    and ensure_path_exists helper was added in helpers.py

--- goatoolsgui/helpers.py
+++ goatoolsgui/helpers.py
@@ -4,19 +4,17 @@
 import sys
 sys.path.append('/home/vagrant/goatools_alpha')
 import os
-# os.chdir('/var/www/projects/gosite/data_files/')
 # End comment
 
 import goatools
 from goatools_alpha import gosubdag_grouper as gta
 from datetime import datetime
 import json
-from django.core.cache import cache
-
 
 # Here to speed development
 os.chdir('/var/www/projects/gosite/data_files/')
-FLAT_GROUPS = gta.GrouperDflts()
+# FLAT_GROUPS = gta.GrouperDflts()
+FLAT_GROUPS = None
 os.chdir('/var/www/projects/gosite/')
 # /speed development
 
@@ -25,7 +23,6 @@ os.chdir('/var/www/projects/gosite/')
 def submit_gos(form, form_file_path=None):
   goids = str(form.get('goids')).translate(None, ',').split()
   filename = str(form.get('filename'))
-  # print(goids)
 
   """
   Any file that is passed will be a 'sections' file and must be passed to
@@ -35,11 +32,14 @@ def submit_gos(form, form_file_path=None):
     print('')
     print('There is a file for submit_gos')
     print('')
+    # print(form_file_path)
+    # print('')
+    # print(open(form_file_path, 'r').read())
     # TODO: Check for 'flat' or 'section' key
     # Return found key or file to download?
-    data = gta.wr_xlsx_gos_sectionsfile(filename, goids, form_file_path, grprdflt=FLAT_GROUPS)
-    print(data['flat'])
-    return data['flat']
+    data = gta.wr_xlsx_gos_sectionsfile(filename, goids, form_file_path)
+    print(data)
+    return data
 
     """
     Text files with a list of GO id's are parsed client side and submitted
@@ -49,7 +49,8 @@ def submit_gos(form, form_file_path=None):
     print('\nNo file sent to submit_gos\n')
     # TODO: Check for 'flat' or 'sections' key
     # Return found key or the file to download
-    data = gta.wr_xlsx_gos(filename, goids, grprdflt=FLAT_GROUPS)
+    data = gta.wr_xlsx_gos(filename, goids)
+    print(data)
     return data
 
 # Accepts 'flat' or 'sections' key in submit_gos() data object
@@ -73,12 +74,52 @@ def nts_to_json(data):
   # print(response_object['flat'][0]) # Access one table row
 
 #
-# def make_sections_file(section_name, goids, grprdflt=FLAT_GROUPS):
-#   start_time = datetime.now()
-#   group_obj = gta.GrouperUserGos(section_name, goids)
-#   group_obj.grprobj.wr_txt_section_hdrgos(section_name + '_hdrgos.txt')
-#   end_time = datetime.now()
-#   print '\n'
-#   print end_time - start_time
-#   print '\n'
-#   return
+def make_sections_file(user, group_name, goids, grprdflt=FLAT_GROUPS):
+  directory = "./data_files/tmp/sections_txt/"  + str(user) + '/'
+  filename = directory + group_name
+
+  if not os.path.exists(directory):
+    try:
+      os.makedirs(directory)
+    except:
+      pass
+
+  # Make the file
+  gta.wr_sections_txt(filename, goids, grprdflt=FLAT_GROUPS)
+
+  # Create JSON representation of file
+  content = open(filename, 'rb').read()
+  lines = content.split('\n')
+  test_obj = {}
+
+  group = ''
+  section = ''
+  for line in lines:
+    if '# GROUP' in line:
+      group = line.strip('# GROUP NAME:')
+      test_obj['groups'] = { group: { 'line': line, 'sections': {} } }
+    elif '# SECTION' in line:
+      section = line.strip('# SECTION:')
+      test_obj['groups'][group]['sections'][section] = { 'line': line, 'gos': [] }
+      print section
+    else:
+      test_obj['groups'][group]['sections'][section]['gos'].append(line)
+
+  return filename
+  # return test_obj
+
+
+def get_list(data):
+  for key in data:
+    if key == 'list_2d':
+      return data['list_2d']
+    else:
+      return data['list_1d']
+
+def ensure_path_exists(path):
+  if not os.path.exists(path):
+    try:
+      os.makedirs(path)
+    except:
+      print "\nCouldn't make directory\n"
+    return
--- goatoolsgui/models.py
+++ goatoolsgui/models.py
@@ -3,15 +3,29 @@ import os
 
 from django.db import models
 from jsonfield import JSONField
+from django.conf import settings
+from django.core.files.base import ContentFile
+from goatools_alpha.socket_client_n_server import GrouperSocketClient as Socket
+from .helpers import ensure_path_exists
+
+# This causes no file ./manage.py error
+# from goatools_alpha.grouper_socket import GrouperSocketClient as Socket
+
+def user_directory_path(instance, filename):
+  return 'data_files/tmp/{0}/{1}'.format(instance.id, filename)
 
 # Create your models here.
 # TODO: Should I Use the ORM provided by Models
 class GoIds(models.Model):
-  sections_file = models.FileField(upload_to='tmp/', null=True)
   go_ids = models.TextField(max_length=40000, default='GO:1234567')
   file_out_name = models.CharField(max_length=30, null=True)
+  sections_file = models.FileField(upload_to=user_directory_path, null=True)
+  xlsx_file = models.FileField(upload_to=user_directory_path, null=True)
   xlsx_data = models.TextField(max_length=80000)
   json_data = JSONField()
+
+  # Do I need to add
+  # sections = models.CharField
 
   def __str__(self):
     return self.go_ids
@@ -25,3 +39,143 @@ class GoIds(models.Model):
     print(self.file_out_name)
     os.remove(self.file_out_name)
     super(GoIds, self).delete()
+
+# Returns 'list_2d' in object with sections and related goids
+  def get_sections(self, sections):
+    # sections = [('section1', ["GO:0007049", "GO:0022402", "GO:0022403", "GO:0000279", "GO:0006259"]),
+    #             ('section2', ["GO:0007049", "GO:0022402", "GO:0022403", "GO:0000279", "GO:0006259"])]
+
+    rq = 'get_sections_goids'
+    goids = self.go_ids.split(',')
+
+    data = Socket().send_request(
+      {
+        'rqid':self.id,
+        'rq':rq,
+        'usrgos':goids,
+        'sections':sections
+      }
+    )
+
+    for key in data:
+      print key
+    return data
+
+  def get_sections_details(self, sections):
+    rq = 'get_sections_nts'
+    goids = self.go_ids.split(',')
+
+    data = Socket().send_request(
+      {
+        'rqid':self.id,
+        'rq':rq,
+        'usrgos':goids,
+        'sections':sections
+      }
+    )
+
+    # return data['list_2d'] or data['nts_2d']
+    return data['list_2d']
+
+# Why does this expect a sections-file
+  def make_sections_file(self, sections):
+    '''
+    Create a default sections file if user hasn't uploaded a file
+    '''
+    if self.sections_file:
+      print '\nThere is a sections file'
+    else:
+      path = user_directory_path(self, '')
+      ensure_path_exists(path)
+      self.sections_file.save('default_sections.txt', ContentFile(''))
+
+    '''
+    Call the socket server to create the sections file
+    '''
+    rq = 'wr_txt_sections'
+    goids = self.go_ids.split(',')
+    # TODO: Is this safe in production?
+    file = settings.BASE_DIR + '/' + self.sections_file.name
+
+    data = Socket().send_request(
+      {
+        'rqid':self.id,
+        'rq':rq,
+        'usrgos':goids,
+        # 'sections_file':file,
+        'outfile':file
+      }
+    )
+    return
+
+
+  def get_xlsx_data(self, sections):
+    '''
+    Send request to Socket server for data
+    '''
+    rq = 'get_xlsx_data'
+
+    goids = self.go_ids.split(',')
+    data = Socket().send_request(
+      {
+        'rqid':self.id,
+        'rq':rq,
+        'usrgos':goids,
+        'sections':sections
+      }
+    )
+
+    '''
+    Return either 2d or 1d list of xlsx data
+    '''
+    if sections:
+      print('\nget_xlsx_data returning 2d list\n')
+      return data['nts_2d']
+    else:
+      print('\nget_xlsx_data returning 1d list\n')
+    return data['nts_1d']
+
+
+# Sections don't seem to be making a difference
+  def wr_xlsx_data(self, sections):
+    # sections = [('section1', ["GO:0007049", "GO:0022402", "GO:0022403", "GO:0000279", "GO:0006259"]),
+    #             ('section2', ["GO:0007049", "GO:0022402", "GO:0022403", "GO:0000279", "GO:0006259"])]
+    # Returns ['status', 'hdrs', 'list_len', list_1d', 'rqid', 'nts_1d', 'rq']
+    # If sections are provided 1d keys are 2d
+    print('\nTesting\n')
+
+    # self.xlsx_file.save('testing_contentfile.xlsx', ContentFile(data['nts_1d']))
+
+    '''
+    Create the directories and file to prevent
+    'No such file or directory' error in GOATOOLS
+    '''
+    directory = settings.BASE_DIR + '/' + user_directory_path(self, '')
+    file = directory + self.file_out_name
+    ensure_path_exists(directory)
+
+
+    '''
+    Call the socket server to make the Excel file
+    '''
+    rq = 'wr_xlsx'
+    goids = self.go_ids.split(',')
+
+    data = Socket().send_request(
+      {
+        'rqid':self.id,
+        'rq':rq,
+        'usrgos':goids,
+        'sections':sections,
+        'outfile':file
+      }
+    )
+
+    '''
+    Set the file on the model
+    '''
+    self.xlsx_file = file
+    print '\nXLSX FILE\n'
+    print self.xlsx_file
+
+    return
