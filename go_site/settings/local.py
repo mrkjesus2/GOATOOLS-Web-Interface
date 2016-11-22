dc64ce19e91d90569b7f022ee66e10be263fb65f
Mark <markweig@gmail.com>
2016-11-22 14:09:12 -0500

    Remove SECRET_KEY from all past commits

---
+++ go_site/settings/local.py
@@ -0,0 +1,19 @@
+from go_site.settings.base import *
+from go_site.settings.secret_settings.py import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# INSTALLED_APPS += (
+#   'livereload'
+# )
+
+# MIDDLEWARE += (
+#   'livereload.middleware.LiveReloadScript'
+# )
+
+LIVERELOAD_HOST = 'gosite.dev'
---
+++ go_site/settings/production.py
@@ -0,0 +1,64 @@
+from go_site.settings.base import *
+from go_site.settings.secret_settings import *
+import os
+
+# A misguided? attempt at detecting the server ip address and adding to ALLOWED_HOSTS
+# IP_ADDR = os.popen("ifconfig eth0 | grep inet | awk'{print $2}' | sed -e s/.*://", "r").read().rstrip()
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+  '.gosite.dev',
+  '.gosite.dev.',
+  '.gosite.production',
+  '.gosite.production.',
+  # '.' + IP_ADDR,
+  # '.' + IP_ADDR + '.',
+  '.159.203.178.3',
+  '.159.203.178.3.',
+]
+
+# INSTALLED_APPS += (
+#   'django.contrib.staticfiles' ## Can I get away with this?
+# )
+
+# Allow the browser to help detect XSS attacks
+SECURE_BROWSER_XSS_FILTER=True
+
+# Prevent site from being loaded in iframes
+# X_FRAME_OPTIONS=DENY
+
+# Client-side Javascript can't access the cookie
+# CSRF_COOKIE_HTTPONLY=True # This prevented AJAX calls from working properly
+
+#
+# Future Implementation
+#
+# If we serve files uploaded by users
+# SECURE_CONTENT_TYPE_NOSNIFF=True ## Look into this one
+
+# If HTTPS needs to be enabled
+# SECURE_HSTS_SECONDS=
+# SECURE_SSL_REDIRECT=True ## Look into this one
+# SESSION_COOKIE_SECURE=True
+# CSRF_COOKIE_SECURE=True
+
+# To implement email:
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# DEFAULT_FROM_EMAIL
+# SERVER_EMAIL
+
+
+# DEPLOY-TODO: Configure Apache allowed hosts
+# DEPLOY-TODO: Database/Cache servers only accept connections from application server?
+# DEPLOY-TODO: Make sure uploaded files aren't executed
+
+
+# For Performance
+CONN_MAX_AGE = 600
+# Cached template loader?
+
+# DEPLOY-TODO: Enable logging
+# DEPLOY-TODO: PYTHONHASHSEED=random in Apache config
