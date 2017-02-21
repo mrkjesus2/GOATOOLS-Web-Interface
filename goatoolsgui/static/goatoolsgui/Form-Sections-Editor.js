a1befc8cff2789ecba0a70775afedd695ea01d9a
Mark <markweig@gmail.com>
2017-02-21 13:38:42 -0500

    Fix Editor - Make more semantic with unordered lists

--- goatoolsgui/static/goatoolsgui/Form-Sections-Editor.js
+++ goatoolsgui/static/goatoolsgui/Form-Sections-Editor.js
@@ -5,7 +5,7 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
 
 (function() {
   'use strict';
-  
+
   /*
     Private Variables
    */
@@ -92,19 +92,20 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
       }
 
       lineObj.sections.forEach(function(section) {
-        var sectionContainer = makeSectionContainer(section[0]);
+        var $sectionContainer = makeSectionContainer(section[0]);
 
         // Append the section name line
-        sectionContainer.append(makeEditorLine(section[0], 'section'));
-
+        $sectionContainer.append(makeEditorLine(section[0], 'section'));
+        $sectionContainer.append($('<ul/>'));
         // Add goid lines
         section[1].forEach(function(goid) {
           if (goid.length > 0) {
-            sectionContainer.append(makeEditorLine(goid, 'goid'));
+            var $line = makeEditorLine(goid, 'goid');
+            $sectionContainer.children().last().append($line);
           }
         });
 
-        $el.append(sectionContainer);
+        $el.append($sectionContainer);
       });
     },
 
@@ -177,7 +178,7 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
  */
   function makeSectionContainer(line) {
     var cssValidRegex = /[~!@$%^&*()+=,.\/';:"?><[\]\\{}|`#]/g;
-    var container = $('<div/>', {
+    var $container = $('<div/>', {
       id: line ? line.replace(cssValidRegex, '') : '',
       class: 'editor__section-container'
     })
@@ -186,7 +187,7 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
         'Goatools.Form.Sections.Editor.sectionDropOver(event, this)'
       );
 
-    return container;
+    return $container;
   }
 
   function makeEditorLine(item, type) {
@@ -196,8 +197,10 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
     });
 
     if (type === 'goid') {
-      $line.attr({
+      $line = $('<li/>', {
         id: item.substr(0, 10),
+        class: 'editor__goid-line',
+        html: item,
         draggable: 'true'
       })
         .on('dragstart', goidDragStart)
@@ -218,6 +221,7 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
     var $newSection = $line.children(0);
 
     $container.append($line);
+    $container.append($('<ul/>'));
     $container.insertBefore($('.editor__section-container')[0]);
 
     selectEditable($newSection[0]);
@@ -310,10 +314,16 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
     ev.preventDefault();
 
     var id = ev.dataTransfer.getData('text');
+    var newEl = document.getElementById(id);
     var toEl = ev.toElement;
 
-    // Insert element and remove drag-over state
-    el.insertBefore(document.getElementById(id), ev.toElement.nextSibling);
+    if (ev.target.className.includes('editor__section-line')) {
+      el.lastChild.insertBefore(newEl, el.lastChild.firstChild);
+    } else {
+      el.lastChild.insertBefore(newEl, ev.target.nextSibling);
+    }
+
+    // Remove drag-over state
     toEl.className = toEl.className.replace('editor__goid-line--drag-over', '');
 
     // Stop the scroll loop used while dragging
@@ -324,276 +334,3 @@ Goatools.Form.Sections = Goatools.Form.Sections || {};
 })();
 
 Goatools.Form.Sections.Editor.init();
-/**
- * Sections File Editor
- * @param {string} file A string representing the sections.txt file
- * @return {array} An array of strings representing each section
- */
-// function divideTxtFileBySection(file) {
-//   // console.log('Divide Text File');
-//   var lines = file.split('# SECTION: ');
-//   return lines;
-// }
-
-
-// /**
-//  * Create a container element for a section
-//  * @param  {string} line The section name
-//  * @return {HTMLElement}      Container div
-//  */
-// function makeSectionContainer(line) {
-//   // var container = document.createElement('div');
-//   var cssValidRegex = /[~!@$%^&*()+=,.\/';:"?><[\]\\{}|`#]/g;
-//   //
-//   // if (line) {
-//   //   container.id = line.replace(cssValidRegex, '');
-//   // }
-//   // container.className = 'editor__section-container';
-//   // container.setAttribute('ondrop', 'sectionDropOver(event, this)');
-//
-//   var container = $('<div/>', {
-//     id: line ? line.replace(cssValidRegex, '') : '',
-//     class: 'editor__section-container',
-//   })
-//     .attr('ondrop', 'sectionDropOver(event, this)');
-//
-//   return container;
-// }
-
-// /**
-//  * Create an element for an Editor line
-//  * @param  {string} item The line text
-//  * @param  {string} type Type of line that is created 'group', 'section', 'goid'
-//  * @return {HTMLElement}      The line that is created
-//  */
-// function makeEditorLine(item, type) {
-//   var $line = $('<div/>', {
-//     class: 'editor__' + type + '-line',
-//     html: type === 'section' ? '# SECTION: ' + item : item
-//   });
-//
-//   if (type === 'goid') {
-//     $line.attr({
-//       id: item.substr(0, 10),
-//       draggable: 'true'
-//     })
-//       .on('dragstart', goidDragStart)
-//       .on('dragend', goidDragEnd);
-//   }
-//   return $line;
-// }
-
-// /**
-//  * Creates HTML representation of the array created from sections.txt
-//  * @param  {array} sectionsArray Array of strings representing the .txt file
-//  * @return {undefined}
-//  */
-// function createTxtFileHtml(sectionsArray) { // eslint-disable-line no-unused-vars
-//   // console.log('Create File HTML');
-//   var $el = $('#editor__file');
-//   // var el = document.getElementById('editor__file');
-//   // var fragment = document.createDocumentFragment();
-//   var lineObj = Goatools.File.Sections.parseLines(sectionsArray);
-//
-//   // Clear previous information
-//   // el.innerHTML = '';
-//   $el.html('');
-//   if (lineObj.group) {
-//     $el.append(makeEditorLine(lineObj.group, 'group'));
-//   }
-//
-//   lineObj.sections.forEach(function(section) {
-//     var sectionContainer = makeSectionContainer(section[0]);
-//
-//     // Append the section name line
-//     sectionContainer.append(makeEditorLine(section[0], 'section'));
-//
-//     section[1].forEach(function(goid) {
-//       if (goid.length > 0)
-//         sectionContainer.append(makeEditorLine(goid, 'goid'));
-//     });
-//
-//     // fragment.append(sectionContainer);
-//     $el.append(sectionContainer);
-//   });
-//
-//   // el.append(fragment);
-// }
-
-/*
-* Trying HTML drag and drop
-*/
-
-// // Variables needed for scroll function
-// var scrollId = 0;
-// var stopScroll = true;
-// var cursor; // Set by dragOver
-// var height = window.innerHeight; // Query once, improve performance
-
-/**
- * Allow scroll while dragging lines
- * @param  { num } timestamp  Return id from requestAnimationFrame
- * @return { undefined }
- */
-// function scroll() {
-//   if (cursor < 210) {
-//     // Scroll up faster
-//     window.scrollBy(0, -10);
-//   } else if (cursor > height - 50) {
-//     // Scroll down faster
-//     window.scrollBy(0, 10);
-//   } else if (cursor < 260) {
-//     // Scroll up slow
-//     window.scrollBy(0, -5);
-//   } else if (cursor > height - 100) {
-//     // Scroll down slow
-//     window.scrollBy(0, 5);
-//   }
-//
-//   if (!stopScroll) {
-//     scrollId = window.requestAnimationFrame(scroll);
-//   }
-// }
-
-/**
- * Drag and Drop Handlers
- */
-
-/**
- * Handle the event fired when dragging starts
- * @param  {object} ev The event object
- * @return {undefined}
- */
-// function goidDragStart(ev) {
-//   console.log(ev);
-//   // Add event listeners to 'editor__section-container'
-//   var containers = document.getElementsByClassName('editor__section-container');
-//
-//   for (var i = 0; i < containers.length; i++) {
-//     var container = containers[i];
-//
-//     container.addEventListener('dragenter', sectionDragEnter, false);
-//     container.addEventListener('dragleave', sectionDragLeave, false);
-//     container.addEventListener('dragover', sectionDragOver, false);
-//   }
-//
-//   // Set the data to be transferred by dragging
-//   // ev.dataTransfer.setData('text/plain', ev.target.id);
-//   ev.originalEvent.dataTransfer.setData('text/plain', ev.target.id);
-//
-//   // Allow the modal content to scroll while dragging
-//   stopScroll = false;
-//   scrollId = window.requestAnimationFrame(scroll);
-// }
-
-/**
- * Handle the event fired when dragging ends
- * @param  {object} ev The event object
- * @return {undefined}
- */
-// function goidDragEnd() {
-//   var containers = document.getElementsByClassName('editor__section-container');
-//
-//   // Unbind event listeners when not dragging
-//   for (var i = 0; i < containers.length; i++) {
-//     var container = containers[i];
-//
-//     container.removeEventListener('dragenter', sectionDragEnter, false);
-//     container.removeEventListener('dragleave', sectionDragLeave, false);
-//     container.removeEventListener('dragover', sectionDragOver, false);
-//   }
-//   // End the scrolling loop
-//   cursor = null;
-//   stopScroll = true;
-//   window.cancelAnimationFrame(scroll);
-// }
-
-/**
- * Section Drag Handlers
- */
-
- /**
-  * Handle the event fired when a line is dropped on 'editor__section-container'
-  * @param  {object} ev The event object
-  * @param  {HTMLElement} el The element that fired the event
-  * @return {undefined}
-  */
- // function sectionDropOver(ev, el) { // eslint-disable-line no-unused-vars
- //   console.time('Drop Over Handler');
- //   // TODO BUG: If there are duplicate GOID's it grabs the first one/not the dragged one
- //   ev.preventDefault();
- //   var id = ev.dataTransfer.getData('text');
- //
- //   el.insertBefore(document.getElementById(id), ev.toElement.nextSibling);
- //
- //   var target = ev.toElement;
- //   var targetParent = target.parentElement;
- //
- //   target.className = target.className
- //                      .replace(' editor__goid-line--drag-over', '');
- //   targetParent.className = targetParent.className
- //                           .replace(' editor__section-container--drag-over', '');
- //
- //   // Stop the scroll loop used while dragging
- //   stopScroll = true;
- //   window.cancelAnimationFrame(scrollId);
- //   Goatools.Form.Sections.Editor.addWarning();
- //   console.timeEnd('Drop Over Handler');
- // }
-
-// /**
-//  * Handle the event fired when entering a 'editor__section-container' element
-//  * @param  {object} ev The event object
-//  * @param  {HTMLElement} el The element that fired the event
-//  * @return {undefined}
-//  */
-// function sectionDragEnter(ev) {
-//   ev.preventDefault();
-//   // if (el.className.indexOf('editor__section-container--drag-over') === -1) {
-//   //   el.className += ' editor__section-container--drag-over';
-//   // }
-//
-//   if (ev.target !== this) {
-//     // Add goid line classname if it doesn't exist
-//     if (ev.target.className.indexOf('editor__goid-line--drag-over') === -1) {
-//       ev.target.className += ' editor__goid-line--drag-over';
-//     }
-//   } else {
-//     // Add classname if it doesn't exist
-//   }
-// }
-
-/**
- * Handle the event fired when leaving a 'editor__section-container' element
- * @param  {objec} ev The event object
- * @param  {HTMLElement} el The element that fired the event
- * @return {undefined}
- */
-// function sectionDragLeave(ev) {
-//   if (ev.target === this) {
-//     ev.target.className = ev.target.className.replace(
-//       ' editor__section-container--drag-over', ''
-//     );
-//     // console.log(ev.target);
-//   } else {
-//     // console.log('Left a section');
-//     // console.log("Left a line");
-//     ev.target.className = ev.target.className.replace(
-//       ' editor__goid-line--drag-over', ''
-//       );
-//   }
-// }
-
-/**
- * Handle the dragover event fired while over 'editor__section-container'
- * @param  {event} ev The event object
- * @param  {HTMLElement} el The element that fired the event
- * @return {undefined}
- */
-// function sectionDragOver(ev) {
-//   ev.preventDefault();
-//   ev.originalEvent.dataTransfer.dropEffect = 'move';
-//
-//   // Update the cursor position for the scroll loop
-//   cursor = ev.pageY;
-// }
